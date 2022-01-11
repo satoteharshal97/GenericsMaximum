ccb4e5358ac69ab7c012461d794414184df41aec
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-11 07:55:32 +0530

    Added - Find maximum number of float objects

--- src/GetMaximum.java
+++ src/GetMaximum.java
@@ -11,8 +11,21 @@ public class GetMaximum {
         return max;
     }
 
+    public static Float maximumFloat(Float a, Float b, Float c) {
+
+        Float max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.printf("Max of %d, %d and %d is %d\n\n",
-                3, 4, 5, maximum(3, 4, 5));
+        System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n",
+                6.6, 8.8, 7.7, maximumFloat((float) 6.6, (float) 8.8, (float) 7.7));
+
     }
 }
