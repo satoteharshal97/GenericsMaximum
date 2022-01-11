8260945ea62b0b8b149bf0a6142edddafdeacce7
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-11 07:49:10 +0530

    Added - Find maximum number of integer objects

--- src/GetMaximum.java
+++ src/GetMaximum.java
@@ -1,6 +1,18 @@
 public class GetMaximum {
+    public static Integer maximum(Integer x, Integer y, Integer z) {
+
+        Integer max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Generics");
+        System.out.printf("Max of %d, %d and %d is %d\n\n",
+                3, 4, 5, maximum(3, 4, 5));
     }
 }
