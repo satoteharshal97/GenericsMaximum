cbc0fb6b83eb95f2aa9d1c2084f0799c71944ddf
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-14 18:54:28 +0530

    Added - Refactor one and two

--- src/GetMaximum.java
+++ src/GetMaximum.java
@@ -1,40 +1,25 @@
 public class GetMaximum {
-    public static Integer maximum(Integer x, Integer y, Integer z) {
 
-        Integer max = x;
-        if (y.compareTo(max) > 0) {
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+
+        if(y.compareTo(max) > 0) {
             max = y;
         }
-        if (z.compareTo(max) > 0) {
+
+        if(z.compareTo(max) > 0) {
             max = z;
         }
         return max;
     }
 
-    public static Float maximumFloat(Float a, Float b, Float c) {
+    public static void main(String args[]) {
+        System.out.printf("Max of %d, %d and %d is %d\n\n",
+                3, 4, 5, maximum( 3, 4, 5 ));
 
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-    public static String maximum(String x, String y, String z) {
+        System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n",
+                6.6, 8.8, 7.7, maximum( 6.6, 8.8, 7.7 ));
 
-        String max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
-
-    public static void main(String[] args) {
         System.out.printf("Max of %s, %s and %s is %s\n","peach",
                 "apple", "banana", maximum("peach", "apple", "banana"));
     }
